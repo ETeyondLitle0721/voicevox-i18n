6ecce29046ecda7e6482c4e854d3756c469317ae
ETeyondLitle0721 <eteyondlitle@gmail.com>
2026-09-15 20:36:26 +0800

    更新标题

--- src/components/Menu/MenuBar/MenuBar.vue
+++ src/components/Menu/MenuBar/MenuBar.vue
@@ -86,7 +86,7 @@ const titleText = computed(
     (isMultiEngineOffMode.value ? " - マルチエンジンオフ" : "") +
     (defaultEngineAltPortTo.value != null
       ? ` - Port: ${defaultEngineAltPortTo.value}`
-      : "") + "- i18n",
+      : "") + "- i18n&l10n",
 );
 
 // FIXME: App.vue内に移動する
